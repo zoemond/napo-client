626e6ee212f5cf2713a4e8da86b214506247d866
koki <myaddressisthis123@gmail.com>
2020-04-28 03:16:55 +0900

    chore: style: prettier, eslint

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,13 @@ import { hot } from "react-hot-loader";
 import { Button } from "@material-ui/core";
 
 class App extends React.Component<{}, undefined> {
-    public render() {
-        return (
-            <Button variant="contained" color="primary" >hello material-ui</Button>
-        );
-    }
+  render(): React.ReactNode {
+    return (
+      <Button variant="contained" color="primary">
+        hello material-ui
+      </Button>
+    );
+  }
 }
 
 declare let module: object;

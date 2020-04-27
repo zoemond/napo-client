496495b05d7d98f0826c4263fcac2802cba98a14
koki <myaddressisthis123@gmail.com>
2020-04-28 02:28:15 +0900

    refactor: starterのsrcコードを削除

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,18 +2,10 @@ import * as React from "react";
 import { hot } from "react-hot-loader";
 import { Button } from "@material-ui/core";
 
-const reactLogo = require("./../assets/img/react_logo.svg");
-import "./../assets/scss/App.scss";
-
 class App extends React.Component<{}, undefined> {
     public render() {
         return (
-            <div className="app">
-                <h1>Hello World!</h1>
-                <p>Foo to the barz</p>
-                <img src={reactLogo.default} height="480" />
-                <Button variant="contained" color="primary" >hello material-ui</Button>
-            </div>
+            <Button variant="contained" color="primary" >hello material-ui</Button>
         );
     }
 }
